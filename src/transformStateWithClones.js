ec4eb90790dc10e121cd8eb07376be33414c4bce
chips <sanchelo0822@gmail.com>
2025-07-11 12:44:41 +0300

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let curentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        curentState = { ...curentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        curentState = { ...curentState };
+
+        for (const key of action.keysToRemove) {
+          delete curentState[key];
+        }
+        break;
+
+      case 'clear':
+        curentState = {};
+        break;
+    }
+    history.push(curentState);
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
